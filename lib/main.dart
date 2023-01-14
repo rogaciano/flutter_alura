e6dfc1fa28d232be54e649adc49e477cab0e81d3
rogaciano <rogaciano@gmail.com>
2023-01-13 22:14:11 -0300

    desenvolvido o incremento do botão nivel ++

--- lib/main.dart
+++ lib/main.dart
@@ -40,28 +40,57 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
   const Task(this.nome, {Key? key}) : super(key: key);
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+
+  int nivel = 0;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(color: Colors.blue, height: 140,
-          ),
-          Container(color: Colors.white, height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(color: Colors.blue, height: 140,
+            ),
+            Column(
               children: [
-                Container(color: Colors.black26, width: 72, height: 100,
+                Container(color: Colors.white, height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(color: Colors.black26, width: 72, height: 100,
+                      ),
+                      Container(child: Text(widget.nome,
+                        style:
+                        TextStyle(fontSize: 24,
+                        overflow: TextOverflow.ellipsis),
+                      ),
+                      width: 200,),
+                      ElevatedButton(onPressed:  () {
+                        setState(() {
+                            nivel++;
+                            print(nivel);
+                        });
+                    }, child: Icon(Icons.arrow_drop_up))
+                    ],
+                  ),
                 ),
-                Text(this.nome),
-                ElevatedButton(onPressed:  () {}, child: Icon(Icons.arrow_drop_up))
+                Text('Nível: $nivel', style: TextStyle(color: Colors.white),)
               ],
-            ),),
-        ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
